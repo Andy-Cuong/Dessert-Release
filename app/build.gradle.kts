b5554fdb034b98c6a362a13c6f4cecaa8b64dff2
Andy Cuong <nguyenduccuong11102003@gmail.com>
2024-11-03 21:43:49 +0200

    Add solution code and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,16 +16,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
     namespace = "com.example.dessertrelease"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dessertrelease"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -55,7 +56,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.9"
     }
     packaging {
         resources {
@@ -66,15 +67,17 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
--- app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
+++ app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
@@ -17,40 +17,59 @@ package com.example.dessertrelease.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.dessertrelease.DessertReleaseApplication
 import com.example.dessertrelease.R
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.example.dessertrelease.data.UserPreferencesRepository
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-/*
+/**
  * View model of Dessert Release components
  */
-class DessertReleaseViewModel() : ViewModel() {
+class DessertReleaseViewModel(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
+    /**
+     * UI states access for various [DessertReleaseUiState]
+      */
+    val uiState: StateFlow<DessertReleaseUiState> =
+        userPreferencesRepository.isLinearLayout.map { isLinearLayout ->
+            DessertReleaseUiState(isLinearLayout = isLinearLayout)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = DessertReleaseUiState()
+        )
 
-    private val _uiState = MutableStateFlow(DessertReleaseUiState())
-
-    // UI states access for various [DessertReleaseUiState]
-    val uiState: StateFlow<DessertReleaseUiState> = _uiState
-
-    /*
+    /**
      * [selectLayout] change the layout and icons accordingly and
      * save the selection in DataStore through [userPreferencesRepository]
      */
     fun selectLayout(isLinearLayout: Boolean) {
-        _uiState.value = DessertReleaseUiState(isLinearLayout)
+        viewModelScope.launch {
+            userPreferencesRepository.saveLayoutPreference(isLinearLayout = isLinearLayout)
+        }
     }
 
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                DessertReleaseViewModel()
+                val application = this[APPLICATION_KEY] as DessertReleaseApplication
+                val userPreferencesRepository = application.userPreferencesRepository
+                DessertReleaseViewModel(userPreferencesRepository = userPreferencesRepository)
             }
         }
     }
 }
 
-/*
+/**
  * Data class containing various UI States for Dessert Release screens
  */
 data class DessertReleaseUiState(
